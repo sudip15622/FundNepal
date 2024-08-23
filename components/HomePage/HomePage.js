1e40cfb45a6b169655663068a6f46522ad13deeb
sudip <sudeeplamichhane18@gmail.com>
2024-08-23 20:09:32 +0545

    reset the default app

---
+++ components/HomePage/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import HomePageClient from './Client/HomePageClient'
+
+const HomePage = () => {
+  return (
+    <>
+        <HomePageClient/>
+    </>
+  )
+}
+
+export default HomePage
