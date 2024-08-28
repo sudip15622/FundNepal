bdca3c75aaefc7f22078b366723faf1166fb9c7b
sudip <sudeeplamichhane18@gmail.com>
2024-08-28 20:44:12 +0545

    started creating login page

---
+++ app/sign-in/page.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import SignIn from '@/components/SignIn/SignIn'
+
+const page = () => {
+  return (
+    <>
+      <SignIn />
+    </>
+  )
+}
+
+export default page
---
+++ components/SignIn/SignIn.js
@@ -0,0 +1,17 @@
+"use client"
+import React from 'react'
+import "./SignIn.css";
+
+const Login = () => {
+  return (
+    <main className='login-container'>
+      <div className="blue-box"></div>
+      <div className="red-box"></div>
+      <div className="white-box">
+
+      </div>
+    </main>
+  )
+}
+
+export default Login
